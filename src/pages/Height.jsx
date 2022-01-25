a8d3bc355e7c6fa7ed30e780c6a58f8ed09b3887
Arthores <arthurolis41@gmail.com>
2022-01-24 22:34:46 -0300

    Height logic working

--- src/pages/Height.jsx
+++ src/pages/Height.jsx
@@ -3,7 +3,62 @@ import { Link } from 'react-router-dom';
 import './Height.css';
 
 class Height extends Component {
+  constructor() {
+    super();
+  
+    this.state = {
+      heightSelect: 'feets',
+      heightInput: '',
+      convertedNum: '',
+      pharseNum: '',
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleChangeSelect = this.handleChangeSelect.bind(this);
+    this.convertLogic = this.convertLogic.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  }
+
+  handleChangeSelect(e) {
+    this.setState({
+      [e.target.name]: e.target.value,
+      heightInput: '',
+      pharseNum: '',
+      convertedNum: '',
+    });
+  }
+
+  convertLogic() {
+    const { heightInput, heightSelect } = this.state;
+
+    if(heightSelect === 'feets') {
+      const saveNum = Number(heightInput);
+      const feetCalc = saveNum * 0.3048;
+      const fixedResult = feetCalc.toFixed(2);
+      this.setState({
+        pharseNum: heightInput,
+        convertedNum: fixedResult,
+        heightInput: '',
+      })
+    } else if(heightSelect === 'meters') {
+      const saveNum = Number(heightInput);
+      const metersCalc = saveNum * 3.28084;
+      const fixedResult = metersCalc.toFixed(2);
+      this.setState({
+        pharseNum: heightInput,
+        convertedNum: fixedResult,
+        heightInput: '',
+      })
+    }
+  }
+
   render() {
+    const { heightSelect, heightInput, convertedNum } = this.state;
     return (
       <body>
         <section>
@@ -14,22 +69,39 @@ class Height extends Component {
           </div>
           <div className="input-box">
             <label htmlFor="heightSelect">
-              <select name="heightSelect" id="heightSelect" className="heightSelect">
-                <option value="meters">Metros</option>
+              <select
+                name="heightSelect"
+                id="heightSelect"
+                className="heightSelect"
+                value={ heightSelect }
+                onChange={ this.handleChangeSelect }
+              >
                 <option value="feets">Pés</option>
+                <option value="meters">Metros</option>
               </select>
+            </label>
+            <label htmlFor="heightInput">
+              <input 
+                type="number"
+                name="heightInput"
+                id="heightInput"
+                className="heightInput"
+                value={ heightInput }
+                onChange={ this.handleChange }
+              />
             </label>
           </div>
           <div className="height-buttons-box">
             <button
               type='button'
+              onClick={ this.convertLogic }
             >
               Converter
             </button>
           </div>
           <div className="height-dinamic-pharse">
             <span className="height-pharse">
-              Lógica sem implementação
+              { convertedNum }
             </span>
           </div>
           <div className="home-footer">
