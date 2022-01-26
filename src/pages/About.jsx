111a4ca7d13ef4adf54a81243feedfe677338313
Arthores <arthurolis41@gmail.com>
2022-01-26 00:56:01 -0300

    Projeto funcionando para demosntraçao

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,23 +1,43 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import './About.css'
 
 export class About extends Component {
   render() {
     return (
       <body>
         <section>
-          <div className="explication-text">
+          <div className="explication-text-box">
             <p className='explication'>
-              HI
+              Ola, tudo bem? Me chamo, Arthur. <br/><br/>
+
+              O projeto de Conversor foi pensado com uma ideia de ser um aplicativo que reunia 
+              tanto algumas medidas mais buscadas no google para se converter, mais um convertor de moedas selecionadas
+              para o BRL(real Brasileiro).<br/>
+
+              Usando o FrameWork React, com puro javaScript, CSS3 e um pouco de HTML
+              foi póssivel dar forma a esta aplicação de forma na qual ficasse fluida e responsiva em aparelhos mobile*. <br/><br/><br/>
+
+              *Responsivo completamente em aparelhos com resolução igual ou parecida com Iphones 6, 7, 8(Normal e suas versões Plus), X (Resolução aproximada de 1334x750);
+              <br/>
+              *Tela de Conversão de moedas se encontra inativa por problemas com a API e o calculo feito na hora de converter;
+              <br/>
+              Disponibilizarei o link do repositorio do projeto para consulta do codigo e alterações de quem se sentir na vontade. :)
+              <br/>
+              <br/>
+              <a href="https://github.com/Arthores/conversor" className='conversor-link'> Conversor Repositorio</a>
             </p>
           </div>
           <div className='links-box'>
-            <a href="mailto:arthurolis41@gmail.com" ><img src="https://img.shields.io/badge/Gmail-D14836?style=for-the-badge&logo=gmail&logoColor=white" alt="Gmail Link"/></a>
-            <a href="https://www.linkedin.com/in/arthur-oliver/" ><img src="https://img.shields.io/badge/LinkedIn-0077B5?style=for-the-badge&logo=linkedin&logoColor=white" alt="Linkedin Logo"/></a>
-            <a href="https://instagram.com/harthuroli" ><img src="https://img.shields.io/badge/Instagram-E4405F?style=for-the-badge&logo=instagram&logoColor=white" alt="Instagram Logo"/></a>
-            <a href="https://github.com/Arthores" ><img src="https://img.shields.io/badge/GitHub-100000?style=for-the-badge&logo=github&logoColor=white" alt="GitHub Logo"/></a>
+            Como me achar:
+            <div className="cointainer-links">
+              <a href="https://github.com/Arthores" ><img src="https://img.shields.io/badge/GitHub-100000?style=for-the-badge&logo=github&logoColor=white" alt="GitHub Logo" className='img-link'/></a>
+              <a href="mailto:arthurolis41@gmail.com" ><img src="https://img.shields.io/badge/Gmail-D14836?style=for-the-badge&logo=gmail&logoColor=white" alt="Gmail Link" className='img-link'/></a>
+              <a href="https://www.linkedin.com/in/arthur-oliver/" ><img src="https://img.shields.io/badge/LinkedIn-0077B5?style=for-the-badge&logo=linkedin&logoColor=white" alt="Linkedin Logo" className='img-link'/></a>
+              <a href="https://instagram.com/harthuroli" ><img src="https://img.shields.io/badge/Instagram-E4405F?style=for-the-badge&logo=instagram&logoColor=white" alt="Instagram Logo" className='img-link'/></a>
+            </div>
           </div>
-          <div>
+          <div className='home-footer'>
             <Link to="/" className='home-link'>
               Home
             </Link>
